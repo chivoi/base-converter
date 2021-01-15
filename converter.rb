5e4b4dafbd97690910aee8a09b86c4203ef8ec1c
chivoi <a.lastovirya@gmail.com>
2021-01-15 11:59:54 +1100

    initial commit. created converter.rb file + spec

---
+++ converter.rb
@@ -0,0 +1,20 @@
+def to_lower_base (number, base)
+  converted_number = []
+	loop do
+		if number < base
+      converted_number < number
+      break
+		else
+			digit = number % base
+			converted_number << digit
+			number = number / base
+		end
+	end
+	return converted_number.reverse.join
+end
+
+def to_higher_base (number, base)
+  converted_number = []
+  
+
+end
