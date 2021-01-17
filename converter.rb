fc8d925ce9c0974cf7f81c7162cefc30448b6379
chivoi <a.lastovirya@gmail.com>
2021-01-17 13:49:41 +1100

    added method base_to_decimal and passing tests for it

--- converter.rb
+++ converter.rb
@@ -1,4 +1,4 @@
-# convert decimal to bases
+# convert from decimal to bases
 
 def decimal_to_base (decimal, base)
   # this is to get the chars for the numbers larger than 9
@@ -6,3 +6,14 @@ def decimal_to_base (decimal, base)
   converted_array = decimal.digits(base).reverse
   result_string = converted_array.map{|digit| digit >=9 ? (chars[digit-10]) : digit}.join
 end
+
+# convert from bases to decimal
+def base_to_decimal (number, base)
+  number.to_i(base.to_i)
+end
+
+# p "Enter number"
+# number = gets.strip
+# p "Enter base"
+# base = gets.strip.to_i
+# p base_to_decimal(number, base)
--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -7,16 +7,25 @@ describe "Convert decimal to bases" do
   it "converts decimal to base 5" do
 		expect(decimal_to_base(344, 5)).to eq("2334")
   end
-  it "converts decimal to base 9" do
-		expect(decimal_to_base(344, 9)).to eq("422")
-	end
-	it "converts 1" do
-		expect(decimal_to_base(1, 2)).to eq("1")
-	end
-	it "converts 0" do
-		expect(decimal_to_base(0, 2)).to eq("0")
-  end
   it "converts decimal to hex" do
     expect(decimal_to_base(10, 16)).to eq("A")
-  end
+	end
+	it "converts decimal to base 26" do
+		expect(decimal_to_base(1589743, 26)).to eq("3CBHP")
+	end
+end
+
+describe "From bases to decimal" do
+	it "converts base 3 to decimal" do
+		expect(base_to_decimal("1021", 3)).to eq(34)
+	end
+	it "converts base 2 to decimal" do
+		expect(base_to_decimal("1111", 2)).to eq(15)
+	end
+	it "converts hex to decimal" do
+		expect(base_to_decimal("F25", 16)).to eq(3877)
+	end
+	it "converts base 26 to decimal" do
+		expect(base_to_decimal("3CBHP", 26)).to eq(1589743)
+	end
 end
